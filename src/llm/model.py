10759722b19327954521b40a06825ad7f85e3464
Akhil Saini <akhilsaini@gmail.com>
2026-08-29 23:37:31 +0530

    llm Config

--- src/llm/model.py
+++ src/llm/model.py
@@ -4,63 +4,13 @@ from transformers import (
     AutoTokenizer,
     AutoModelForCausalLM,
     BitsAndBytesConfig,
+    pipeline,
 )
-from langchain_core.language_models.llms import LLM
-from typing import Any, List, Optional
-from pydantic import Field
+
+from langchain_huggingface import HuggingFacePipeline
+
 
 MODEL_ID = "deepseek-ai/DeepSeek-R1-0528-Qwen3-8B"
-
-
-class CustomDeepSeekLLM(LLM):
-    model: Any = Field(default=None)
-    tokenizer: Any = Field(default=None)
-    max_new_tokens: int = 400
-
-    class Config:
-        arbitrary_types_allowed = True
-
-    model_config = {
-        "arbitrary_types_allowed": True
-    }
-
-    def _call(
-        self,
-        prompt: str,
-        stop: Optional[List[str]] = None,
-        **kwargs: Any,
-    ) -> str:
-        messages = [
-            {"role": "user", "content": prompt}
-        ]
-
-        inputs = self.tokenizer.apply_chat_template(
-            messages,
-            add_generation_prompt=True,
-            tokenize=True,
-            return_dict=True,
-            return_tensors="pt",
-        ).to(self.model.device)
-
-        outputs = self.model.generate(
-            **inputs,
-            max_new_tokens=self.max_new_tokens,
-            do_sample=False,
-        )
-
-        generated_tokens = outputs[
-            0,
-            inputs["input_ids"].shape[-1]:
-        ]
-
-        return self.tokenizer.decode(
-            generated_tokens,
-            skip_special_tokens=True,
-        )
-
-    @property
-    def _llm_type(self) -> str:
-        return "custom_deepseek"
 
 
 def load_llm():
@@ -82,4 +32,17 @@ def load_llm():
         device_map="auto",
     )
 
-    return CustomDeepSeekLLM(model=model, tokenizer=tokenizer)
+    text_pipeline = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        max_new_tokens=300,
+        do_sample=False,
+    )
+
+    llm = HuggingFacePipeline(
+    pipeline=text_pipeline
+)
+
+    return llm, tokenizer, model
+    
