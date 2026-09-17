6117f918d23ebed6f588de6ddede0e121ca68160
Akhil Saini <akhilsaini@gmail.com>
2026-09-17 08:42:53 +0530

    addd

--- src/llm/__init__.py
+++ src/llm/__init__.py
@@ -1,6 +1,6 @@
 from importlib import import_module
 
-_factory = import_module(".factory", __name__)
+_factory = import_module(".provider", __name__)
 create_llm = _factory.create_llm
 list_providers = _factory.list_providers
 
