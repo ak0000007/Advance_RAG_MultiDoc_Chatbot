c247d03987b20b36da64ac4b9396dc700644139b
Akhil Saini <akhilsaini@gmail.com>
2026-09-17 10:15:24 +0530

    add fallback1

--- src/llm/__init__.py
+++ src/llm/__init__.py
@@ -1,10 +1,13 @@
-from importlib import import_module
-
-_factory = import_module(".provider", __name__)
-create_llm = _factory.create_llm
-list_providers = _factory.list_providers
+from .provider import (
+    create_llm,
+    list_providers,
+    ModelProviderError,
+    ModelRateLimitError,
+)
 
 __all__ = [
     "create_llm",
     "list_providers",
+    "ModelProviderError",
+    "ModelRateLimitError",
 ]
